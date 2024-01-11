07439161f689e25a0ef1cdfaf8a1d5895c71ed7c
Tzuyang Tsai <14104502+shiyou0130011@users.noreply.github.com>
2024-01-11 11:35:40 +0800

    setup month input value

--- src/nu-month-input.js
+++ src/nu-month-input.js
@@ -6,6 +6,19 @@ import {NuInputElement} from "./modules/nu-input-element.js"
 class NuMonthInput extends NuInputElement{
 	toString(){return `[object NuMonthInput]`}
 	#defaultValue = "---- --"
+	/** The local for formating date */
+	#formatter = new Intl.DateTimeFormat(
+		navigator.userLanguage || navigator.language || navigator.languages[0] || "default",
+		{
+			year: "numeric",
+			month: "long", 
+		}
+	)
+
+	#year = null
+	#month = null
+
+
 	constructor() {
 		super()
 
@@ -51,6 +64,30 @@ class NuMonthInput extends NuInputElement{
 		shadowRoot.appendChild(document.importNode(temp.content, true))
 	}
 
+	get value(){
+		if(this.#year === null || this.#month === null){
+			return ""
+		}
+		return `0000${this.#year}`.substr(-4) + "-" `00${this.#month}`.substr(-2)
+	}
+	set value(v){
+		if(!v){
+			this.#year = null
+			this.#month = null
+			this.shadowRoot.querySelector("#display-value").innerHTML = this.#defaultValue
+			return 
+		}
+
+		if(v.match(/\d{4}-\d{2}/) || v.match(/\d{4}-\d{2}-\d{2}((T| )[\d:\.Z\+]*|)/)){
+			this.#year = Number(v.substring(0, 4))
+			this.#month = Number(v.substring(5, 8))
+
+			this.shadowRoot.querySelector("#display-value").innerHTML = this.#formatter.format(
+				new Date(this.#year, this.#month - 1)
+			)		
+		}
+
+	}
 
 	static get observedAttributes() {
 		return [... super.observedAttributes, "max", "min"]
@@ -58,6 +95,11 @@ class NuMonthInput extends NuInputElement{
 
 	attributeChangedCallback(name, oldValue, newValue){
 		super.attributeChangedCallback(name, oldValue, newValue)
+		switch(name){
+			case "value":
+				this.value = newValue
+				break
+		}
 	}
 }
 
