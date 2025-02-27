942a7237f896fb2c1b1495fe3f35bf9b72426ea0
Tzuyang Tsai <14104502+shiyou0130011@users.noreply.github.com>
2025-02-27 15:50:55 +0800

    #2 撰寫 checkbox 的 HTML 與樣式

--- src/nu-checkbox.js
+++ src/nu-checkbox.js
@@ -8,7 +8,78 @@ class NuCheckboxElement extends NuInputElement{
 		let shadowRoot = this.attachShadow({mode: 'open'})
 		let temp = document.createElement("template")
 
-		temp.innerHTML =``
+		temp.innerHTML =`
+		<style>
+			:host{
+				--label-font-size: 1rem;
+			
+				--checkbox-width: 2.5rem;
+				--checkbox-height: var(--label-font-size);
+				--checkbox-outline-size: 0.2rem;
+				--checkbox-background: gray;
+				--checkbox-inner-background: white;
+				
+				--checkbox-text-space: .5rem;
+			}
+
+			#checkbox-content{
+				display: flex;
+				column-gap: var(--checkbox-text-space);
+				height: var(--checkbox-height);
+				
+				align-items: center;
+			}
+			#checkbox-outer{
+				width: var(--checkbox-width);
+				background-color: var(--checkbox-background);
+				height: var(--checkbox-height);
+				
+				outline-width: var(--checkbox-outline-size);
+				outline-style: solid;
+				outline-offset: 0;
+				outline-color: var(--checkbox-background);
+				
+				
+				border-radius: var(--checkbox-height);
+			}
+			#checkbox-inner{
+				background-color: var(--checkbox-inner-background);
+				height: var(--checkbox-height);
+				width: var(--checkbox-height);
+				display: block;
+				border-radius: 50%;
+				
+				margin-left: 0;
+				
+				transition: margin .5s;
+			}
+			label{
+				font-size: var(--label-font-size);
+				font-family: sans-serif
+			}
+			::slotted([slot=label]){
+				font-size: var(--label-font-size);
+				font-family: sans-serif
+			}
+			input[type=checkbox]{
+				display: none
+			}
+			
+			input[type=checkbox]:checked ~ #checkbox-outer #checkbox-inner{
+				margin-left: auto;		
+			}
+			
+		</style>
+		<div id="checkbox-content">
+			<input type="checkbox" id="cb"/>
+			<div id="checkbox-outer">
+				<span id="checkbox-inner"></span>
+			</div>
+			<label for="cb">
+				<slot name="label"></slot>
+			</label>
+		</div>
+		`
 		shadowRoot.appendChild(document.importNode(temp.content, true))
 	}
 }
